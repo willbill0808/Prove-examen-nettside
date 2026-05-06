006d48325b60641c2c64e83c6b6979e2a8735433
willbill0808 <williamlrosewell08@gmail.com>
2026-05-06 22:34:02 +0200

    fix: only httpS set false

--- routes/users.js
+++ routes/users.js
@@ -28,7 +28,7 @@ router.post("/log_in", async (req, res) => {
         const cookie = await make_jwt(userName)
         res.cookie("Token", cookie, {
             httpOnly: true,
-            secure: true,
+            secure: false,
             sameSite: "strict",
         });
 
