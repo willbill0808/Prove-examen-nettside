c3f29d69d9e00f41b61ba7dedba6f60d3f48b0b2
willbill0808 <williamlrosewell08@gmail.com>
2026-05-08 09:25:07 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -23,7 +23,7 @@ var connection = mysql.createConnection({
 
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp.hotmail.com",
     port: 587,
     secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
     auth: {
