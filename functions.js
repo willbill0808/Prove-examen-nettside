386f7d9085d76d0932acf22dabadd861db4a882e
Rosewell William Laugerud <williamlrosewell08@gmail.com>
2026-06-03 10:38:36 +0200

    forbedret phone handling

--- functions.js
+++ functions.js
@@ -217,7 +217,7 @@ async function Authentication(User, plainPassword) { // funksjonen som sjekker o
 connection.query(`CREATE TABLE IF NOT EXISTS ${UserTable} (
     ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     User_name VARCHAR(50) NOT NULL UNIQUE,
-    Phone_number INT(15) NOT NULL UNIQUE,
+    Phone_number VARCHAR(15) NOT NULL UNIQUE,
     Email VARCHAR(100),
     Password VARCHAR(255) NOT NULL
 );`) // Lager bruker databasen
