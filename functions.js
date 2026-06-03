97f2b0762128a490af0eaed0c15c008b08803a8a
Rosewell William Laugerud <williamlrosewell08@gmail.com>
2026-06-03 12:19:41 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -161,7 +161,8 @@ async function phone_validate(Phone_number) { // funksjonen som ser om et bruker
     console.log(phone_checked)
     console.log(parseFloat(phone_checked))
 
-    if (parseFloat(phone_checked) === NaN) {console.log("parse is true")
+    if (Number.isNaN(parseFloat(phone_checked))) {
+        console.log("parse is true");
         return 1}
 
     const [rows] = await connection.query(`SELECT Phone_number FROM ${UserTable} WHERE Phone_number = ?;`, [Phone_number])
